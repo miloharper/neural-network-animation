2ecc595928c1fd76bcc91af6efc7df4426933f02
Milo Spencer-Harper <miloharper@gmail.com>
2015-08-11 20:29:11 +0800

    Skip every 500 cycles to speed up the video.

--- parameters.py
+++ parameters.py
@@ -12,7 +12,7 @@ output_y_position = 15
 
 # Parameters for the video
 frames_per_second = 1
-iterations_per_frame = 100
+iterations_per_frame = 500
 video_file_name = "neural_network.mp4"
 metadata = dict(artist="Milo Spencer-Harper", title="Neural Network")
 
