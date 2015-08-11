d206f17e6e29e59cc8e9c59cd3032354280b6401
Milo Spencer-Harper <miloharper@gmail.com>
2015-08-11 17:20:16 +0800

    Changed background colour to black, removed axis labels and zoomed into the plot.

--- video.py
+++ video.py
@@ -6,25 +6,30 @@ def generate_writer():
     FFMpegWriter = animation.writers['ffmpeg']
     writer = FFMpegWriter(fps=parameters.frames_per_second, metadata=parameters.metadata)
     fig = pyplot.figure()
+    fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
+    pyplot.xlim(0, parameters.width)
+    pyplot.ylim(0, parameters.height)
+    axis = pyplot.gca()
+    axis.set_axis_bgcolor('black')
+    axis.axes.get_xaxis().set_visible(False)
+    axis.axes.get_yaxis().set_visible(False)
     return fig, writer
 
 
 def new_frame():
-    pyplot.clf()
-    pyplot.xlim(0, parameters.width)
-    pyplot.ylim(0, parameters.height)
+    pyplot.cla()
 
 
 def annotate_frame(i, e, average_error):
-    pyplot.text(1, parameters.height - 1, "Iteration " + str(i + 1), fontsize=12)
-    pyplot.text(1, parameters.height - 2, "Example " + str(e + 1), fontsize=12)
+    pyplot.text(1, parameters.height - 1, "Iteration " + str(i + 1), fontsize=12, color='white')
+    pyplot.text(1, parameters.height - 2, "Example " + str(e + 1), fontsize=12, color='white')
     if average_error:
-        pyplot.text(7, parameters.height - 1, "Average Error " + str(average_error) + "%", fontsize=12)
+        pyplot.text(7, parameters.height - 1, "Average Error " + str(average_error) + "%", fontsize=12, color='white')
         error_bar(average_error)
 
 
 def error_bar(average_error):
-    border = pyplot.Rectangle((7, parameters.height - 3), 10, 1, color=(0, 0, 0), fill=False)
+    border = pyplot.Rectangle((7, parameters.height - 3), 10, 1, color='white', fill=False)
     pyplot.gca().add_patch(border)
-    rectangle = pyplot.Rectangle((7, parameters.height - 3), 10 * average_error / 100, 1, color=(1, 0, 0))
+    rectangle = pyplot.Rectangle((7, parameters.height - 3), 10 * average_error / 100, 1, color='red')
     pyplot.gca().add_patch(rectangle)
